c9a3ef3781315022725e4008b6ac98cc2d2f3017
Danny Clark <dev@dclark.us>
2015-01-21 21:10:29 -0500

    Updates

--- src/git2csv.sh
+++ src/git2csv.sh
@@ -2,8 +2,13 @@
 
 # git2csv.sh - Transfer git metadata to csv comma seperated value format
 #              (for import to sqlite3 later)
+#              Where "metadata" means "the subset of metadata the author 
+#              wanted to analyze" :-)
 
 # NOTE: This only works if your repo has no files or folders with spaces.
+#       Also, I'm sure there are a lot of other edge case failure modes.
+# This project has a far better (and more complex) git-fast-export parser:
+# github.com/maxandersen/jbosstools-gitmigration/tree/master/git_fast_filter
 
 
 export LC_ALL=C
@@ -18,7 +23,7 @@ while read GFE; do
 	ITEMS=($GFE)
 	if [[ ${ITEMS[0]} == 'M' ]]; then
 		printf "$MARK,${ITEMS[2]},${ITEMS[3]}\n" >> gfe.csv
-	elif [[ ${ITEMS[0]} == 'mark' ]]; then
+	elif [[ ${ITEMS[0]} == 'mark' && ${ITEMS[1]:0:2} == :[0-9] ]]; then
 		MARK="${ITEMS[1]:1}"
 		echo "MARK: $MARK"	
 	fi
