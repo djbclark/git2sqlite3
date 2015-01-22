9dbec5b5b47e0d0e41947f896403c532c389ceee
Danny Clark <dev@dclark.us>
2015-01-21 19:58:04 -0500

    Update to work with fast export
    Should be dramatically smaller as only changes go to .csv file - should
    be able to “fill in” via SQL magic.

--- src/git2csv.sh
+++ src/git2csv.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
- 
-# Note: It would probably be a lot faster to use git fast-export and an edited
-# github.com/maxandersen/jbosstools-gitmigration/tree/master/git_fast_filter
-# but this was already done...
- 
+
+# git2csv.sh - Transfer git metadata to csv comma seperated value format
+#              (for import to sqlite3 later)
+
+# NOTE: This only works if your repo has no files or folders with spaces.
+
+
 export LC_ALL=C
-export COUNT="1"
-git log --full-history --pretty=format:"%T" > S.git-log
-echo >> S.git-log
-test -f S.csv && rm S.csv
-while read TREE; do
-  git ls-tree --full-tree -r $TREE \
-  | fgrep ' blob ' \
-  | awk -v C="$COUNT" '{print C","$3","$4}' >> S.csv
-  COUNT="$(expr $COUNT + 1)"
-done < S.git-log
+
+git fast-export --no-data --all > gfe.txt
+echo >> gfe.txt
+
+test -f gfe.csv && rm gfe.csv
+
+unset MARK
+while read GFE; do
+	ITEMS=($GFE)
+	if [[ ${ITEMS[0]} == 'M' ]]; then
+		printf "$MARK,${ITEMS[2]},${ITEMS[3]}\n" >> gfe.csv
+	elif [[ ${ITEMS[0]} == 'mark' ]]; then
+		MARK="${ITEMS[1]:1}"
+		echo "MARK: $MARK"	
+	fi
+done < gfe.txt
